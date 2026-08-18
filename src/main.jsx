e15a5c4be8f96a61df1bd8429494a0cdcbf28e91
subeyda707 <s.farah@bham.ac.uk>
2026-08-19 00:53:06 +0100

    Wrap App component with HashRouter and DataProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { DataProvider } from './DataContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <DataProvider><App /></DataProvider>
+    </HashRouter>
+  </React.StrictMode>
 )
